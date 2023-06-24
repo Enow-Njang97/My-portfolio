5706f6912ebe3d677efa6eeba8d36a7c89246b13
Enow Njang <enownjang97@gmail.com>
2023-06-23 20:56:25 -0700

    updated backend

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG = {
+   DB: 'mongodb+srv://enownjang97:GOy25hGq1Gw2vUE4@backend.gf6xfxs.mongodb.net/?retryWrites=true&w=majority'
+    
+}
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
